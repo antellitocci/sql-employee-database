4be79eccec2125a5e627ad8190096f7106cb5f45
raddymaster <antellitocci@gmail.com>
2021-07-04 23:10:05 -0400

    install necessary packages and intial project layout

---
+++ app.js
@@ -0,0 +1,29 @@
+const inquirer = require('inquirer');
+
+//inquirer questions
+//what would you like to do?
+// // view all departments
+// // view all roles
+// // view all employees
+// // add a department
+// // add a role
+// // add an employee
+// // update an employee role
+
+//add a department
+// // enter name of department
+
+//add a role
+// // enter the name
+// // enter the salary
+// // enter the department
+
+//add an employee
+// // enter first name
+// // enter last name
+// // enter role
+// // enter manager
+
+//update an employee role
+// // select employee to update
+// // indicate new role
